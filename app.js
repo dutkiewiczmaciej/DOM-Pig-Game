5a860211584518e0e40ce301e73d373c9ff47ec1
dutkiewiczmaciej <dutkiewiczmaciej@me.com>
2019-08-17 19:22:45 +0200

    add eventlistener for dice image and score

--- app.js
+++ app.js
@@ -10,11 +10,25 @@ GAME RULES:
 */
 
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random()*6) + 1;
-console.log(dice);
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    var dice = Math.floor(Math.random()*6) + 1;
+
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+})
